18a7c4c7d121a21b39a598c195639fa7add41ac0
Jerry200603 <jerry.for.home@foxmail.com>
2023-01-24 23:22:59 +0800

    修复docs/Jerry-MC-Server/join-game页面警告框渲染 继续在该页面测试图片加载优化

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,8 +19,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Jerry12228', // Usually your GitHub org/user name.
-  projectName: 'jerry-docs', // Usually your repo name.
+  // organizationName: 'Jerry12228', // Usually your GitHub org/user name.
+  // projectName: 'jerry-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
